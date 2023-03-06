24ccbae1ebd70c84f5e1e7b621ce323361e3ca07
1360151219 <1360151219@qq.com>
2023-03-06 23:40:59 +0800

    feat: [3.6]视图切换

--- 3DAUVGCSV2/offlineMap/library/test/index.js
+++ 3DAUVGCSV2/offlineMap/library/test/index.js
@@ -1,9 +1,40 @@
-function test1(){
+let timer1, timer2;
+function test1() {
   let i = 114.4375999
-    let y = 0
-    console.log('test');
-    setInterval(() => {
-        showBoatPosition(0, i, 30.5252794, y += 2, 0)
-        i -= 0.00002
-    }, 500)
+  let y = 0
+  addBoat()
+  console.log('test1');
+  timer1 = setInterval(() => {
+    if (!showBoatPosition(0, i, 30.5252794, y += 2, 0)) {
+      clearInterval(timer1)
+    }
+    i -= 0.00002
+  }, 500)
+}
+function test2() {
+  let i = 114.4375999
+  let y = 0
+  addBoat()
+  console.log('test2');
+  timer2 = setInterval(() => {
+    if (!showBoatPosition(1, i, 30.5252794, y -= 2, 0)) {
+      clearInterval(timer2)
+    }
+    i += 0.00002
+  }, 500)
+}
+
+let clicks = 0
+function test3() {
+  if (clicks >= boatPointsArray.length) {
+    clearTrackAll()
+    clearInterval(timer1)
+    clearInterval(timer2)
+  } else {
+    clearTrack(clicks++)
+  }
+}
+
+function test4(id) {
+  switchTrack(id)
 }
--- 3DAUVGCSV2/offlineMap/library/utils/jsb.js
+++ 3DAUVGCSV2/offlineMap/library/utils/jsb.js
@@ -1,5 +1,5 @@
 /**
- * 展示船舶位置
+ * 更新船舶位置
  * @param {*} lng 经度
  * @param {*} lat 纬度
  * @param {*} course 角度
@@ -7,29 +7,56 @@
  */
 function showBoatPosition(id, lng, lat, course, depth) {
   var currentPosition = wgs84tobd09(lng, lat);
-  var currentPoint = new BMap.Point(currentPosition[0], currentPosition[1]);
+  const currentPoint = new BMap.Point(currentPosition[0], currentPosition[1]);
   const boatIcon = boatIconArray[id]
   const boatLabel = boatLabelArray[id]
   const boatMarker = boatMarkerArray[id]
-  if (!boatIcon || !boatLabel || !boatMarker) {
+  const boatPoints = boatPointsArray[id]
+  const boatTrackPolyline = boatTrackPolylineArray[id]
+  if (!boatIcon || !boatLabel || !boatMarker || !boatPoints || !boatTrackPolyline) {
     alert(`id为${id}的艇不存在`)
-    return
+    return false
   }
   boatIcon.setRotation(course);
   boatLabel.setContent(depth.toFixed(2) + "mD");
   boatMarker.setIcon(boatIcon);
   boatMarker.setLabel(boatLabel);
   boatMarker.setPosition(currentPoint);
-  map.addOverlay(boatMarker);
+
   boatPoints.push(currentPoint);
   boatTrackPolyline.setPath(boatPoints);
-  if (boatPoints.length == 1) {
-    // 添加轨迹折线
-    map.addOverlay(boatTrackPolyline);
+  // 只有全部视图或者是当前艇的视图才显示
+  if(activeId === id || activeId === -1){
+    map.addOverlay(boatMarker);
+    if (boatPoints.length == 1) {
+      // 添加轨迹折线
+      map.addOverlay(boatTrackPolyline);
+    }
+  }
+  
+  return true
+}
+
+// 清除指定无人艇的轨迹
+function clearTrack(id) {
+  boatPointsArray[id] = []
+  map.removeOverlay(boatTrackPolylineArray[id]);
+}
+// 清除所有无人艇的轨迹
+function clearTrackAll() {
+  boatPointsArray = []
+  for (let boatTrackPolyline of boatTrackPolylineArray) {
+    map.removeOverlay(boatTrackPolyline);
   }
 }
 
-// function clearTrack() {
-    //     boatPoints = [];
-    //     map.removeOverlay(boatTrackPolyline);
-    // }
+function switchTrack(id) {
+  activeId = id
+  boatTrackPolylineArray.forEach((boatTrackPolyline, index) => {
+    if (id !== index) {
+      map.removeOverlay(boatTrackPolyline);
+    }
+  })
+  map.clearOverlays();
+  map.addOverlay(boatTrackPolylineArray[id])
+}
